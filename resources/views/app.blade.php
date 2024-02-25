ef62d0efabe6068d7e32b9f6dcea5afe7d9000e0
MohdFahd <mohammed.f.alsulami@gmail.com>
2024-02-25 20:58:09 +0300

    Set Everything

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,24 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+         <!-- Default theme -->
+                <!--
+            - favicon
+        -->
+        <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.ico') }}" type="image/x-icon">
+
+        <!--
+            - custom css link
+        -->
+        <link rel="stylesheet" href="{{ asset('assets/css/style-prefix.css') }}">
+
+        <!--
+            - google font link
+        -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet">
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
@@ -18,4 +36,17 @@
     <body class="font-sans antialiased">
         @inertia
     </body>
+
+
+        <!--
+        - custom js link
+    -->
+    <script src="{{ asset('assets/js/script.js') }}"></script>
+
+
+    <!--
+        - ionicon link
+    -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </html>
