1808d915810ff0d876c85349f212c248176577f8
MohdFahd <mohammed.f.alsulami@gmail.com>
2024-03-09 12:53:44 +0300

    Adding users page

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,6 +11,33 @@ class UserController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Dashboard/users');
+        // $users = User::latest()->paginate(10); // Corrected method names: latest() and paginate()
+        // return Inertia::render('Dashboard/users', ['users' => $users]); // Pass $users to the view
+
+
+        $search = request()->input('search');
+        $users = User::query()
+            ->where(function ($query) use ($search) {
+                $query->where('name', 'like', '%'.$search.'%')
+                    ->orWhere('email', 'like', '%'.$search.'%');
+            })
+            ->paginate(5)
+            ->withQueryString()
+            ->through(function ($user) {
+                return [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'role'=> $user->role,
+                    'status' => $user->status,
+                    'img' => $user->img,
+                    'created_at' => $user->created_at->diffForHumans(),
+                ];
+            });
+        return Inertia::render('Dashboard/users', [
+            'users' => $users,
+            'filters' => request()->only(['search']),
+        ]);
+
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role')->default('user');
+            $table->boolean('status')->default(1);
+            $table->string('img')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
